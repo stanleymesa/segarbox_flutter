409e03988f1b7ca9b8c117ece337e5b7d94e1201
Stanley Mesa <83571092+stanleymesa@users.noreply.github.com>
2022-10-26 20:57:46 +0700

    carousel

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -18,10 +19,95 @@ class HomeView extends GetView<HomeController> {
               headerSliverBuilder: (context, innerBoxIsScrolled) => <Widget>[
                 Header(),
               ],
-              body: Container(),
+              body: ListView(
+                children: <Widget>[
+                  SizedBox(
+                    height: 24,
+                  ),
+                  Carousel(controller: controller),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  CarouselIndicator(controller: controller),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  Container(
+                    height: 500,
+                    color: Colors.amber,
+                  ),
+                  Container(
+                    height: 500,
+                    color: Colors.red,
+                  ),
+                ],
+              ),
             ),
             MainAppBar(controller: controller),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class CarouselIndicator extends StatelessWidget {
+  const CarouselIndicator({
+    Key? key,
+    required this.controller,
+  }) : super(key: key);
+
+  final HomeController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: List.generate(
+        controller.carouselImages.length,
+        (index) => Obx(
+          () => AnimatedContainer(
+            duration: 300.milliseconds,
+            margin: EdgeInsets.only(right: 4),
+            width: (controller.carouselIndex.value == index) ? 32 : 8,
+            height: 8,
+            decoration: BoxDecoration(
+              color:
+                  (controller.carouselIndex.value == index) ? green : softGreen,
+              borderRadius: BorderRadius.all(Radius.circular(8)),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Carousel extends StatelessWidget {
+  const Carousel({
+    Key? key,
+    required this.controller,
+  }) : super(key: key);
+
+  final HomeController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return CarouselSlider.builder(
+      options: CarouselOptions(
+        autoPlay: true,
+        onPageChanged: (index, reason) =>
+            controller.carouselIndex.value = index,
+        viewportFraction: 0.9,
+        height: 132,
+      ),
+      itemCount: 3,
+      itemBuilder: (context, index, realIndex) => ClipRRect(
+        borderRadius: BorderRadius.all(Radius.circular(12)),
+        child: Image.asset(
+          controller.carouselImages[index],
+          width: Get.width - 52,
+          fit: BoxFit.cover,
         ),
       ),
     );
