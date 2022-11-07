f6659624dd781428cc3730012777dcaf4150219e
Stanley Mesa <83571092+stanleymesa@users.noreply.github.com>
2022-11-07 14:14:04 +0700

    Profile Page 1.0

--- lib/app/modules/home/views/profile_view.dart
+++ lib/app/modules/home/views/profile_view.dart
@@ -55,29 +55,200 @@ class ProfilePage extends GetView<ProfileController> {
                     fit: BoxFit.cover,
                   ),
                 ),
-              ),
-            ],
-            body: ListView.builder(
-              physics: NeverScrollableScrollPhysics(),
-              itemCount: 4,
-              itemBuilder: (context, index) => Column(
-                children: [
-                  Container(
-                    height: 400,
-                    color: Colors.cyan,
-                  ),
-                  Container(
-                    height: 400,
-                    color: Colors.red,
+                actions: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 20),
+                    child: Icon(Icons.more_vert_rounded),
                   ),
                 ],
               ),
+            ],
+            body: ListView(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: (diameter / 2) + 8,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 24),
+                      child: Text(
+                        'Agus Surmono Pakiyem Tuminah',
+                        style: title,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 4,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 24),
+                      child: Text(
+                        'agussumono@gmail.com',
+                        style: normalDark.copyWith(color: Colors.grey),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Divider(
+                      color: black,
+                      thickness: 2,
+                    ),
+                    SizedBox(
+                      height: 24,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 24),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'My Cart',
+                            style: normalBold,
+                          ),
+                          Icon(
+                            Icons.arrow_forward_ios_rounded,
+                            color: green,
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 24),
+                      child: Divider(
+                        thickness: 2,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 24),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Rating',
+                            style: normalBold,
+                          ),
+                          Icon(
+                            Icons.arrow_forward_ios_rounded,
+                            color: green,
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 24),
+                      child: Divider(
+                        thickness: 2,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 24),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Dark Mode',
+                            style: normalBold,
+                          ),
+                          SizedBox(
+                            width: 50,
+                            height: 20,
+                            child: Obx(
+                              () => Switch(
+                                value: controller.isDarkMode.value,
+                                activeColor: green,
+                                activeTrackColor: Color(0x52000000),
+                                materialTapTargetSize:
+                                    MaterialTapTargetSize.shrinkWrap,
+                                onChanged: (_) =>
+                                    controller.isDarkMode.toggle(),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 24),
+                      child: Divider(
+                        thickness: 2,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 32,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 24),
+                      child: SizedBox(
+                        width: Get.width,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Logout',
+                            style: title.copyWith(color: defaultWhite),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: green,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(8)))),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
-          ProfileImage(
-            profileController: controller,
-          ),
+          // RoundedStackContainer(controller: controller),
+          AnimatedProfileImage(profileController: controller),
         ],
+      ),
+    );
+  }
+}
+
+class RoundedStackContainer extends StatelessWidget {
+  const RoundedStackContainer({
+    Key? key,
+    required this.controller,
+  }) : super(key: key);
+
+  final ProfileController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(
+      () => Positioned(
+        top: max(defaultStackTop - controller.scrollOffset.value, 0),
+        child: AnimatedOpacity(
+          duration: 300.milliseconds,
+          opacity:
+              (defaultStackTop - controller.scrollOffset.value <= 56) ? 0 : 1,
+          child: Container(
+            height: 20,
+            width: Get.width,
+            decoration: BoxDecoration(
+                color: defaultWhite,
+                borderRadius: BorderRadius.vertical(top: Radius.circular(20))),
+          ),
+        ),
       ),
     );
   }
@@ -119,7 +290,18 @@ class AnimatedProfileImage extends StatelessWidget {
               : 0,
           decoration: BoxDecoration(
             shape: BoxShape.circle,
-            color: Colors.amber,
+            color: defaultWhite,
+            border: Border.all(
+              color: defaultWhite,
+              width: 3,
+            ),
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.all(Radius.circular(diameter)),
+            child: Image.asset(
+              'assets/image/face.jpg',
+              fit: BoxFit.cover,
+            ),
           ),
         ),
       ),
