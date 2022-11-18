922d08409e4564d4f365c419935760c22955ad9a
Stanley Mesa <83571092+stanleymesa@users.noreply.github.com>
2022-11-18 13:59:02 +0700

    Maps 2.0

--- lib/app/modules/maps/views/maps_view.dart
+++ lib/app/modules/maps/views/maps_view.dart
@@ -20,16 +20,113 @@ class MapsView extends GetView<MapsController> {
         titleSpacing: 0,
         elevation: 0.2,
       ),
-      body: Obx(
-        () => GoogleMap(
-          initialCameraPosition: CameraPosition(
-            target: controller.currentLatLng.value,
-            zoom: 15,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 100),
+        child: FloatingActionButton(
+          backgroundColor: green,
+          onPressed: () => controller.getCurrentPosition(),
+          child: Icon(Icons.location_on_rounded),
+        ),
+      ),
+      body: Stack(
+        children: [
+          Obx(
+            () => GoogleMap(
+              initialCameraPosition: CameraPosition(
+                target: controller.currentLatLng.value,
+                zoom: 15,
+              ),
+              onTap: (latLng) {
+                controller.goTo(latLng);
+              },
+              onMapCreated: (mapController) =>
+                  controller.completer.complete(mapController),
+              markers: controller.markers.toSet(),
+              zoomControlsEnabled: false,
+            ),
           ),
-          onTap: (latLng) {},
-          onMapCreated: (mapController) =>
-              controller.completer.complete(mapController),
-          markers: controller.markers.toSet(),
+          InfoWindow(controller: controller),
+          Positioned(
+            child: Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: 24,
+                vertical: 16,
+              ),
+              width: Get.width,
+              color: defaultWhite,
+              child: ElevatedButton(
+                onPressed: () {
+                  Get.back();
+                },
+                child: Text(
+                  'Choose Address',
+                  style: normalBold.copyWith(color: defaultWhite),
+                ),
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: green,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8)))),
+              ),
+            ),
+            bottom: 0,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class InfoWindow extends StatelessWidget {
+  const InfoWindow({
+    Key? key,
+    required this.controller,
+  }) : super(key: key);
+
+  final MapsController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(
+      () => AnimatedPositioned(
+        top: (controller.isWindowClosed.value) ? -150 : 16,
+        left: 16,
+        right: 16,
+        duration: 500.milliseconds,
+        child: Card(
+          elevation: 8,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(12))),
+          child: Container(
+            padding: EdgeInsets.all(16),
+            width: Get.width,
+            decoration: BoxDecoration(
+              color: defaultWhite,
+              borderRadius: BorderRadius.all(Radius.circular(12)),
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Text(
+                    (controller.currentPlacemark.value.street != null)
+                        ? '${controller.currentPlacemark.value.street}, ${controller.currentPlacemark.value.locality}, ${controller.currentPlacemark.value.subAdministrativeArea}, ${controller.currentPlacemark.value.administrativeArea}, ${controller.currentPlacemark.value.postalCode}, ${controller.currentPlacemark.value.country}'
+                        : 'Location Not Found',
+                    style: normalDark,
+                  ),
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                GestureDetector(
+                  onTap: () => controller.isWindowClosed.value = true,
+                  child: Icon(
+                    Icons.close_rounded,
+                    color: green,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
