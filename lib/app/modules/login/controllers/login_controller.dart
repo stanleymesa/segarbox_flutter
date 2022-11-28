f2316b816da7455d23e254f98eb52e44df2b5ebe
Stanley Mesa <83571092+stanleymesa@users.noreply.github.com>
2022-11-28 13:58:47 +0700

    Login & Register Page

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -1,12 +1,113 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class LoginController extends GetxController {
-  //TODO: Implement LoginController
+  // Login Section
+  // Email
+  final emailFocus = FocusNode();
+  final isEmailFocus = false.obs;
+  final emailTextEditingController = TextEditingController();
 
-  final count = 0.obs;
+  // Password
+  final passwordFocus = FocusNode();
+  final isPasswordFocus = false.obs;
+  final passwordTextEditingController = TextEditingController();
+
+  // Sign Up Section
+  // Username
+  final usernameFocus = FocusNode();
+  final isUsernameFocus = false.obs;
+  final usernameTextEditingController = TextEditingController();
+
+  // Email Sign Up
+  final emailSignUpFocus = FocusNode();
+  final isEmailSignUpFocus = false.obs;
+  final emailSignUpTextEditingController = TextEditingController();
+
+  // Phone Sign Up
+  final phoneFocus = FocusNode();
+  final isPhoneFocus = false.obs;
+  final phoneTextEditingController = TextEditingController();
+
+  // Password Sign Up
+  final passwordSignUpFocus = FocusNode();
+  final isPasswordSignUpFocus = false.obs;
+  final passwordSignUpTextEditingController = TextEditingController();
+
+  // Confirmation Password Sign Up
+  final conPasswordSignUpFocus = FocusNode();
+  final isConPasswordSignUpFocus = false.obs;
+  final conPasswordSignUpTextEditingController = TextEditingController();
+
   @override
   void onInit() {
     super.onInit();
+    emailFocus.addListener(() {
+      (emailFocus.hasFocus)
+          ? isEmailFocus.value = true
+          : isEmailFocus.value = false;
+    });
+
+    passwordFocus.addListener(() {
+      (passwordFocus.hasFocus)
+          ? isPasswordFocus.value = true
+          : isPasswordFocus.value = false;
+    });
+
+    usernameFocus.addListener(() {
+      (usernameFocus.hasFocus)
+          ? isUsernameFocus.value = true
+          : isUsernameFocus.value = false;
+    });
+
+    emailSignUpFocus.addListener(() {
+      (emailSignUpFocus.hasFocus)
+          ? isEmailSignUpFocus.value = true
+          : isEmailSignUpFocus.value = false;
+    });
+
+    phoneFocus.addListener(() {
+      (phoneFocus.hasFocus)
+          ? isPhoneFocus.value = true
+          : isPhoneFocus.value = false;
+    });
+
+    passwordSignUpFocus.addListener(() {
+      (passwordSignUpFocus.hasFocus)
+          ? isPasswordSignUpFocus.value = true
+          : isPasswordSignUpFocus.value = false;
+    });
+
+    conPasswordSignUpFocus.addListener(() {
+      (conPasswordSignUpFocus.hasFocus)
+          ? isConPasswordSignUpFocus.value = true
+          : isConPasswordSignUpFocus.value = false;
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    emailFocus.dispose();
+    emailTextEditingController.dispose();
+
+    passwordFocus.dispose();
+    passwordTextEditingController.dispose();
+
+    usernameFocus.dispose();
+    usernameTextEditingController.dispose();
+
+    emailSignUpFocus.dispose();
+    emailSignUpTextEditingController.dispose();
+
+    phoneFocus.dispose();
+    phoneTextEditingController.dispose();
+
+    passwordSignUpFocus.dispose();
+    passwordSignUpTextEditingController.dispose();
+
+    conPasswordSignUpFocus.dispose();
+    conPasswordSignUpTextEditingController.dispose();
   }
 
   @override
@@ -16,5 +117,4 @@ class LoginController extends GetxController {
 
   @override
   void onClose() {}
-  void increment() => count.value++;
 }
--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -49,13 +49,24 @@ class LoginView extends GetView<LoginController> {
                   Expanded(
                     child: TabBarView(
                       children: [
-                        LoginTab(),
-                        SignUpTab(),
+                        LoginTab(controller: controller),
+                        SignUpTab(controller: controller),
                       ],
                     ),
                   )
                 ],
               ),
+              Positioned(
+                top: 268,
+                child: Container(
+                  width: Get.width,
+                  height: 16,
+                  decoration: BoxDecoration(
+                      color: defaultWhite,
+                      borderRadius:
+                          BorderRadius.vertical(top: Radius.circular(16))),
+                ),
+              )
             ],
           ),
         ),
@@ -67,80 +78,98 @@ class LoginView extends GetView<LoginController> {
 class SignUpTab extends StatelessWidget {
   const SignUpTab({
     Key? key,
+    required this.controller,
   }) : super(key: key);
+
+  final LoginController controller;
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      padding: EdgeInsets.symmetric(horizontal: 24, vertical: 24),
-      children: [
-        MyTextfield(
-          label: 'Username',
-          icon: Icon(
-            Icons.person_pin_circle_rounded,
-            color: black,
-            size: 18,
+    return Obx(
+      () => ListView(
+        padding: EdgeInsets.symmetric(horizontal: 24, vertical: 24),
+        children: [
+          MyTextfield(
+            label: 'Username',
+            icon: Icon(
+              Icons.person_pin_circle_rounded,
+              color: (controller.isUsernameFocus.value) ? green : black,
+              size: 18,
+            ),
+            focus: controller.usernameFocus,
+            textEditingController: controller.usernameTextEditingController,
           ),
-        ),
-        SizedBox(
-          height: 16,
-        ),
-        MyTextfield(
-          label: 'Email',
-          icon: Icon(
-            Icons.email_rounded,
-            color: black,
-            size: 18,
+          SizedBox(
+            height: 16,
           ),
-        ),
-        SizedBox(
-          height: 16,
-        ),
-        MyTextfield(
-          label: 'Phone Number',
-          icon: Icon(
-            Icons.phone_rounded,
-            color: black,
-            size: 18,
+          MyTextfield(
+            label: 'Email',
+            icon: Icon(
+              Icons.email_rounded,
+              color: (controller.isEmailSignUpFocus.value) ? green : black,
+              size: 18,
+            ),
+            focus: controller.emailSignUpFocus,
+            textEditingController: controller.emailSignUpTextEditingController,
           ),
-        ),
-        SizedBox(
-          height: 16,
-        ),
-        MyTextfield(
-          label: 'Password',
-          icon: Icon(
-            Icons.lock_rounded,
-            color: black,
-            size: 18,
+          SizedBox(
+            height: 16,
           ),
-        ),
-        SizedBox(
-          height: 16,
-        ),
-        MyTextfield(
-          label: 'Password Confirmation',
-          icon: Icon(
-            Icons.lock_rounded,
-            color: black,
-            size: 18,
+          MyTextfield(
+            label: 'Phone Number',
+            icon: Icon(
+              Icons.phone_rounded,
+              color: (controller.isPhoneFocus.value) ? green : black,
+              size: 18,
+            ),
+            focus: controller.phoneFocus,
+            textEditingController: controller.phoneTextEditingController,
           ),
-        ),
-        SizedBox(
-          height: 48,
-        ),
-        ElevatedButton(
-          onPressed: () {},
-          child: Text(
-            'Sign Up',
-            style: title.copyWith(color: defaultWhite),
+          SizedBox(
+            height: 16,
           ),
-          style: ElevatedButton.styleFrom(
-              backgroundColor: green,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(8)))),
-        )
-      ],
+          MyTextfield(
+            label: 'Password',
+            icon: Icon(
+              Icons.lock_rounded,
+              color: (controller.isPasswordSignUpFocus.value) ? green : black,
+              size: 18,
+            ),
+            focus: controller.passwordSignUpFocus,
+            textEditingController:
+                controller.passwordSignUpTextEditingController,
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          MyTextfield(
+            label: 'Password Confirmation',
+            icon: Icon(
+              Icons.lock_rounded,
+              color:
+                  (controller.isConPasswordSignUpFocus.value) ? green : black,
+              size: 18,
+            ),
+            focus: controller.conPasswordSignUpFocus,
+            textEditingController:
+                controller.conPasswordSignUpTextEditingController,
+          ),
+          SizedBox(
+            height: 48,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text(
+              'Sign Up',
+              style: title.copyWith(color: defaultWhite),
+            ),
+            style: ElevatedButton.styleFrom(
+                backgroundColor: green,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(8)))),
+          )
+        ],
+      ),
     );
   }
 }
@@ -148,47 +177,62 @@ class SignUpTab extends StatelessWidget {
 class LoginTab extends StatelessWidget {
   const LoginTab({
     Key? key,
+    required this.controller,
   }) : super(key: key);
+
+  final LoginController controller;
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      padding: EdgeInsets.symmetric(horizontal: 24, vertical: 24),
-      children: [
-        MyTextfield(
-          label: 'Email',
-          icon: Icon(
-            Icons.email_rounded,
-            color: black,
-            size: 18,
-          ),
+    return Obx(
+      () => GestureDetector(
+        onTap: () {
+          controller.emailFocus.unfocus();
+          controller.passwordFocus.unfocus();
+        },
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 24, vertical: 24),
+          children: [
+            MyTextfield(
+              label: 'Email',
+              icon: Icon(
+                Icons.email_rounded,
+                color: (controller.isEmailFocus.value) ? green : black,
+                size: 18,
+              ),
+              focus: controller.emailFocus,
+              textEditingController: controller.emailTextEditingController,
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            MyTextfield(
+              label: 'Password',
+              icon: Icon(
+                Icons.password_rounded,
+                color: (controller.isPasswordFocus.value) ? green : black,
+                size: 18,
+              ),
+              focus: controller.passwordFocus,
+              textEditingController: controller.passwordTextEditingController,
+            ),
+            SizedBox(
+              height: 48,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                'Login',
+                style: title.copyWith(color: defaultWhite),
+              ),
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: green,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8)))),
+            )
+          ],
         ),
-        SizedBox(
-          height: 16,
-        ),
-        MyTextfield(
-          label: 'Password',
-          icon: Icon(
-            Icons.password_rounded,
-            color: black,
-            size: 18,
-          ),
-        ),
-        SizedBox(
-          height: 48,
-        ),
-        ElevatedButton(
-          onPressed: () {},
-          child: Text(
-            'Login',
-            style: title.copyWith(color: defaultWhite),
-          ),
-          style: ElevatedButton.styleFrom(
-              backgroundColor: green,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(8)))),
-        )
-      ],
+      ),
     );
   }
 }
@@ -198,16 +242,22 @@ class MyTextfield extends StatelessWidget {
     Key? key,
     required this.label,
     required this.icon,
+    required this.focus,
+    required this.textEditingController,
   }) : super(key: key);
 
   final String label;
   final Icon icon;
+  final FocusNode focus;
+  final TextEditingController textEditingController;
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       cursorColor: green,
       style: normalDark.copyWith(color: green),
+      focusNode: focus,
+      controller: textEditingController,
       decoration: InputDecoration(
         hintText: label,
         hintStyle: normalDark,
